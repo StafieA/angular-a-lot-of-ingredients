a05595cfdb13523b61a01c8ec74aea51bef98fbc
Andrei Stafie <andreistafie@gmail.com>
2024-11-02 11:09:59 +0200

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit{
+  recipes:Recipe[] = [
+    new Recipe('Burger',
+      'Taste like a shit',
+      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg'),
+      new Recipe('Calamar',
+        'The price worth',
+        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  ];
+  
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+
+}
