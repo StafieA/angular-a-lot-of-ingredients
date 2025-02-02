ba86f58b7667ed8ef758943a4fc36560c53af2e0
Andrei Stafie <andreistafie@gmail.com>
2025-02-02 15:04:25 +0200

    Pass data with event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent implements OnInit{
-  recipes:Recipe[] = [
-    new Recipe('Burger',
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'Burger',
       'Taste like a shit',
-      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg'),
-      new Recipe('Calamar',
-        'The price worth',
-        'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg'
+    ),
+    new Recipe(
+      'Calamar',
+      'The price worth',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
   ];
-  
-  constructor(){}
-  ngOnInit(): void {
-    
-  }
 
+  constructor() {}
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
