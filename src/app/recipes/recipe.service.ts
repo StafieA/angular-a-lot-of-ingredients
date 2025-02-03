26bf023ff6d143d28ee9810255b33faf09e9685a
Andrei Stafie <andreistafie@gmail.com>
2025-02-03 12:28:05 +0200

    Add ingredients to Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/Ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,12 +8,14 @@ export class RecipeService {
     new Recipe(
       'Burger',
       'Taste like a shit',
-      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg'
+      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Calamar',
       'The price worth',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Garlic', 4)]
     ),
   ];
 
