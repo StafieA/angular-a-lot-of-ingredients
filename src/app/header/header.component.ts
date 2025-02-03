77232ffa1f1060dbfea77195aecb83b8249ce614
Andrei Stafie <andreistafie@gmail.com>
2025-02-03 13:28:33 +0200

    Add navigation to the app and mark active routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  @Output() valueSelected = new EventEmitter<string>();
-  onSelect(value: string) {
-    this.valueSelected.emit(value);
-  }
-}
+export class HeaderComponent {}
