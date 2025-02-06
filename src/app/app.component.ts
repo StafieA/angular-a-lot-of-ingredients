4c17a7510e364efc90847cfc4c37acde9c2e43e2
Andrei Stafie <andreistafie@gmail.com>
2025-02-06 14:50:40 +0200

    Improve code with observable Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recipes-and-ingredients';
-  loadedValue = 'recipe';
-
-  onNavigate(value: string) {
-    this.loadedValue = value;
-  }
 }
