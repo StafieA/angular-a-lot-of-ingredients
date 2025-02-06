9d6177e30493ace55fe5714419bc7eece892d57f
Andrei Stafie <andreistafie@gmail.com>
2025-02-06 15:32:42 +0200

    Allow the selection of items in the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../../shared/Ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css',
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+
   constructor(private shoppingService: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.shoppingService.startEditing.subscribe((index) => {
+      this.editedItemIndex = index;
+      this.editMode = true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   onAddItem(form: NgForm) {
     const value = form.value;
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -28,4 +28,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.changeSubscript.unsubscribe();
   }
+
+  onEditItem(index: number) {
+    this.shoppingListService.startEditing.next(index);
+  }
 }
