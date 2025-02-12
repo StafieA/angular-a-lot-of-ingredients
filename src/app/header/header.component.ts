5534be6fcd6d929dbbbda848e933742c1c54b89f
Andrei Stafie <andreistafie@gmail.com>
2025-02-12 14:49:59 +0200

    PUT request implemente with FIREBASE

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dsStorage: DataStorageService) {}
+  onSave() {
+    this.dsStorage.storeRecipes();
+  }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private httpClient: HttpClientModule) {}
+  constructor(
+    private httpClient: HttpClient,
+    private recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.httpClient
+      .put(
+        'https://shoppingandrecipes-d5dc4-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
