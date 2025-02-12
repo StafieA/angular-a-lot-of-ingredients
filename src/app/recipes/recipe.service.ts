5cbab4b176f02332c573b45fbb56f3c5e76a3cb5
Andrei Stafie <andreistafie@gmail.com>
2025-02-12 15:58:17 +0200

    Transform data to solve a possible bug(lack of ingredients)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,20 +7,22 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Burger',
-      'Taste like a shit',
-      'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Calamar',
-      'The price worth',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Garlic', 4)]
-    ),
-  ];
+  //   private recipes: Recipe[] = [
+  //     new Recipe(
+  //       'Burger',
+  //       'Taste like a shit',
+  //       'https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg',
+  //       [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //     ),
+  //     new Recipe(
+  //       'Calamar',
+  //       'The price worth',
+  //       'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+  //       [new Ingredient('Meat', 1), new Ingredient('Garlic', 4)]
+  //     ),
+  //   ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -26,6 +27,16 @@ export class DataStorageService {
     this.httpClient
       .get<Recipe[]>(
         'https://shoppingandrecipes-d5dc4-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);
